e279fecf4946785e3463d0b66796fd9f4a5c2bfc
Zdeněk Žabokrtský <zabokrtsky@ufal.mff.cuni.cz>
2017-08-21 16:56:38 +0200

    adjectives derived by -sky added to the releasing pipeline (manual annotation partially fixed, but many real-world lemmas seems to be missing in the lemma set)

--- tools/build/cs/1.5/create-1.5.py
+++ tools/build/cs/1.5/create-1.5.py
@@ -61,11 +61,38 @@ def mark_as_compound(node_id):
     derinet._data[node_id] = newnode
     
 
+def find_id(lemma,pos):
+    candidates=[]
+    if pos==None:
+        candidates=derinet.get_ids(lemma)
+    else:
+        candidates=derinet.get_ids(lemma,pos)
+    if len(candidates)==0:
+        print("Error:No node find for lemma="+lemma)
+    elif len(candidates)>1:
+        print("Error:Ambiguous lemma="+lemma)
+    else:
+        return candidates[0]
+
+    
+def create_derivation(filename,child_lemma,child_pos,parent_lemma,parent_pos):
+#    print("Ladim3")
+    try:
+#        print("Ladim4")
+        child_id = find_id(child_lemma,child_pos)
+        parent_id = find_id(parent_lemma,parent_pos)
+        derinet.add_edge_by_ids(child_id,parent_id)
+        print("Derivation successfully added child="+child_lemma+" parent="+parent_lemma+"\t"+filename)
+        
+    except:
+        print("Error: No edge added for child="+child_lemma+" and parent="+parent_lemma+" , either nonexistent or ambiguous")
+
+
 # ---------------- part (a) -------------------
 
 filename = '../../../../data/annotations/cs/2017_08_compounds/all_solitary_compounds'
 fh = open(filename)
-for shortlemma in [line.rstrip('\n') for line in fh]:
+for shortlemma in []:############# [line.rstrip('\n') for line in fh]:
     try:
         node_ids = derinet.get_ids(shortlemma)
         mark_as_compound(node_ids[0])
@@ -76,11 +103,11 @@ for shortlemma in [line.rstrip('\n') for line in fh]:
 
 filename = '../../../../data/annotations/cs/2017_08_compounds/all_compound_clusters'
 fh = open(filename)
-for line in fh:
+for line in []:########3#fh:
     
     tokens = line.rstrip('\n').split(' ')
 
-    print(repr(tokens))
+#    print(repr(tokens))
     
     node_ids = derinet.get_ids(tokens[0])
     mark_as_compound(node_ids[0])
@@ -95,6 +122,28 @@ for line in fh:
             stack.pop()
         else:
             print("Child: "+token+"  Parent: "+stack[-1])
+            create_derivation(filename,token,None,stack[-1],None)
             last_token = token
-        
+
+
+# ---------------- part (c) -------------------
+filename = "../../../../data/annotations/cs/2017_05_sky_cky_ovy/hand-annotated/candidates_sky_bez_kompozit.txt"
+
+fh = open(filename)
+
+for line in fh:
+    columns = line.rstrip('\n').split('\t')
+    child_lemma = columns[0]
+    parent_lemma = columns[1]
+
+#    print("LINE="+line)
+#    print("CHILD="+child_lemma+"  PARENT="+parent_lemma)
+
+    matchObj = re.search(r' [A-Z]: (\w+)',line) # manual correction
+    if matchObj and matchObj.groups:
+        parent_lemma = matchObj.group(1)
+
+    if not parent_lemma=="UNRESOLVED" and not parent_lemma=="":
+        create_derivation(filename, child_lemma, 'A', parent_lemma, None)
+
 derinet.save('derinet-1-5.tsv')
