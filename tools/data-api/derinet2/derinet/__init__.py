0d0085f2f7e55bd8c5a0f029930a3ef9c261d031
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2019-03-14 15:15:32 +0100

    Delete the old v2 API in preparation for the OO one
    We are rewriting the Python-based API once again, because the old-new
    one is lacking usability features provided by an object-oriented
    design. The new one should be easier to program for.

--- tools/data-api/derinet2/derinet/__init__.py
+++
@@ -1,5 +0,0 @@
-from derinet.modules.block import Block
-from .derinet import DeriNet
-from .utils import *
-
-# user-defined error classes
--- tools/data-api/derinet2/derinet/modules/printlexemes.py
+++
@@ -1,7 +0,0 @@
-from .block import Block
-
-class PrintLexemes(Block):
-
-    def process_lexeme(self, lexeme):
-        if lexeme.lemma.startswith('Aa'):
-            print(lexeme.lemma)
