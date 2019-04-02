28f2a7ec12127fe7c62d1454a1ee5c47ffabd06c
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2019-04-02 17:20:35 +0200

    Support parsing the v2-style tuple IDs

--- tools/data-api/derinet2/derinet/utils.py
+++ tools/data-api/derinet2/derinet/utils.py
@@ -1,3 +1,6 @@
+import re
+from typing import Tuple
+
 class DerinetError(Exception):
     """
     The base class for all custom Derinet errors.
@@ -35,6 +38,28 @@ def parse_v1_id(val: str) -> int:
     if num < 0:
         raise ValueError("ID must be a positive integer, is '{}'".format(val))
     return num
+
+
+v2_id_regex = re.compile(r"([0-9]+)\.([0-9]+)")
+def parse_v2_id(val: str) -> Tuple[int, int]:
+    """
+    Parse a textual representation of a DeriNet-2.X ID to a pair of integers.
+
+    :param val: a string containing the ID
+    :return: a tuple of two integers representing the same ID
+    """
+    match = v2_id_regex.fullmatch(val)
+    if match is None:
+        raise ValueError("ID must be a pair of two positive integers separated by a dot, is '{}'".format(val))
+
+    tree_id_str, lex_id_str = match.groups()
+    tree_id, lex_id = int(tree_id_str), int(lex_id_str)
+
+    # Check that the numbers are not overlong, i.e. 0001 is not allowed.
+    assert str(tree_id) == tree_id_str
+    assert str(lex_id) == lex_id_str
+
+    return tree_id, lex_id
 
 
 def format_kwstring(d):
--- tools/data-api/derinet2/test/test_utils.py
+++ tools/data-api/derinet2/test/test_utils.py
@@ -39,6 +39,68 @@ class TestUtils(unittest.TestCase):
         with self.assertRaises(ValueError):
             u.parse_v1_id("A15")
 
+    def test_parse_v2_id(self):
+        self.assertTupleEqual(u.parse_v2_id("0.0"), (0, 0))
+        self.assertTupleEqual(u.parse_v2_id("1.2"), (1, 2))
+        self.assertTupleEqual(u.parse_v2_id("1000000.6000000"), (1000000, 6000000))
+
+        with self.assertRaises(TypeError):
+            # noinspection PyTypeChecker
+            u.parse_v2_id(123.1)
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("0")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("0.0.0")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id(".1")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("1.")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("a.1")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("1.a")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("  0.0")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("0.0  ")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("0.0\n")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id("  ")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id(".")
+
+        with self.assertRaises(ValueError):
+            u.parse_v2_id(" . ")
+
+    def test_parse_v2_id_roundtrip(self):
+        """
+        The following tests may fail with disabled asserts. They are here to
+        ensure that the ID roundtrips properly. Otherwise, you could refer
+        to a lexeme with ID 0.0 using "00.00", or not, depending on the internal
+        representation. We do not want to force a particular representation on
+        implementers.
+        """
+        with self.assertRaises(AssertionError):
+            u.parse_v2_id("00.1")
+
+        with self.assertRaises(AssertionError):
+            u.parse_v2_id("0.01")
+
 
 if __name__ == '__main__':
     unittest.main()
