f514af4d4f5bf83308237fc85c6c0ca5447982bc
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2019-05-14 20:05:14 +0200

    Add tests of the new file format
    The tests finally pass (after disabling the three spurious ones), so
    I can safely commit them. Be aware that there are still unfinished
    design points and many issues all around the code. Even the test suite
    is definitely not production ready yet.

---
+++ tools/data-api/derinet2/test/test_new_format.py
@@ -0,0 +1,300 @@
+import unittest
+import io
+from derinet import Lexicon, DerinetError, DerinetFileParseError
+
+
+class MyTestCase(unittest.TestCase):
+    def test_load_empty(self):
+        db = ""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+        lexicon.load(db_file)
+
+        self.assertEqual(len(list(lexicon.iter_lexemes())), 0)
+
+    def test_load_single_lexeme(self):
+        db = """0.0	0	lexeme							{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+        lexicon.load(db_file)
+
+        self.assertEqual(len(list(lexicon.iter_lexemes())), 1)
+        self.assertEqual(list(lexicon.iter_lexemes())[0].lemma, "lexeme")
+
+    def test_load_missing_json_error(self):
+        db = """0.0	lexeme#N	lexeme	N						"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_missing_lemma_error(self):
+        db = """0.0	lexeme#N		N						{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    @unittest.expectedFailure
+    def test_load_missing_lemid_error(self):
+        """
+        It is not clear at all whether lemids are necessary. Perhaps they can actually be empty?
+        """
+        db = """0.0		lexeme	N						{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_malformed_id_1(self):
+        db = """0	lexeme#N	lexeme	N						{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_malformed_id_2(self):
+        db = """	lexeme#N	lexeme	N						{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_two_lexemes(self):
+        db = """0.0	0	lexeme							{}
+0.1	1	lexemer				0.0			{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+        lexicon.load(db_file)
+
+        self.assertEqual(len(list(lexicon.iter_lexemes())), 2)
+
+    @unittest.expectedFailure
+    def test_load_duplicate_lemid(self):
+        """
+        Duplicate lemids are to be expected, at least in Czech, given that
+        they are constructed as a lemma+tagmask combination. Many homonyms
+        share the same lemid.
+        """
+        db = """0.0	0	lexeme							{}
+0.1	0	lexemer				0.0			{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_duplicate_id_1(self):
+        db = """0.0	0	lexeme							{}
+0.1	1	lexemer				0.0			{}
+0.1	2	lexemes				0.0			{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_duplicate_id_2(self):
+        db = """0.0	0	lexeme							{}
+
+0.0	1	lexemer							{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    @unittest.expectedFailure
+    def test_load_duplicate_load(self):
+        """
+        Since there is no distinguishing feature to the lexeme, there is no way
+        of detecting duplicities. If we used the ID for that, we would effectively
+        disable loading multiple databases into a single Lexicon instance.
+        """
+        db = """0.0	0	lexeme							{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        lexicon.load(db_file)
+        db_file.seek(0)
+
+        with self.assertRaises(DerinetError):
+            lexicon.load(db_file)
+
+        self.assertEqual(len(list(lexicon.iter_lexemes())), 1)
+
+    def test_load_two_dbs(self):
+        db1 = """0.0	0	lexeme							{}"""
+        db2 = """1.0	1	word							{}"""
+
+        lexicon = Lexicon()
+        db_file_1 = io.StringIO(db1)
+        db_file_2 = io.StringIO(db2)
+
+        lexicon.load(db_file_1)
+        lexicon.load(db_file_2)
+
+        self.assertEqual(len(list(lexicon.iter_lexemes())), 2)
+
+    def test_load_conjoined_trees_1(self):
+        """
+        In this test, we check that the loader rejects a file in which
+        two trees are written without a blank line between them.
+        """
+        db = """0.0	0	lexeme							{}
+1.1	1	word							{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_conjoined_trees_2(self):
+        db = """0.0	0	lexeme							{}
+1.1	1	word				0.0			{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_duplicate_id(self):
+        db = """0.0	0	lexeme							{}
+0.0	1	word							{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_self_reference(self):
+        db = """0.0	0	lexeme				0.0			{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_forward_reference(self):
+        db = """0.0	0	lexemer				0.1			{}
+0.1	1	lexeme							{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_out_of_tree_reference(self):
+        """
+        Cross-tree references are only allowed in secondary links, not in primary ones.
+        """
+        db = """0.0	0	lexeme							{}
+
+1.0	1	lexemer				0.0			{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_split_tree_1(self):
+        db = """0.0	0	lexeme							{}
+
+0.1	1	lexemer				0.0			{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_split_tree_2(self):
+        db = """0.0	0	lexeme							{}
+
+0.1	1	lexemer							{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_child_before_parent(self):
+        db = """0.1	0	lexemer				0.0			{}
+0.0	1	lexeme							{}"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        with self.assertRaises(DerinetFileParseError):
+            lexicon.load(db_file)
+
+    def test_load_segmentation(self):
+        db = """7.3	aachenskost#N	aachenskost	N		end=6&morph=aachen&start=0&type=prefix|end=8&start=6|end=11&morph=ost&start=8&type=suffix				{}
+"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        lexicon.load(db_file)
+
+        lexeme = lexicon.get_lexemes("aachenskost")[0]
+        self.assertEqual(len(lexeme.segmentation), 3)
+
+    def test_load_segmentation_noninteger_start(self):
+        db = """7.3	aachenskost#N	aachenskost	N		end=6&morph=aachen&start=0.1&type=prefix|end=8&morph=sk&start=6&type=root|end=11&morph=ost&start=8&type=suffix				{}
+"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        self.assertRaises(DerinetFileParseError, lexicon.load, db_file)
+
+    def test_load_segmentation_noninteger_end(self):
+        db = """7.3	aachenskost#N	aachenskost	N		end=6a&morph=aachen&start=0&type=prefix|end=8&morph=sk&start=6&type=root|end=11&morph=ost&start=8&type=suffix				{}
+"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        self.assertRaises(DerinetFileParseError, lexicon.load, db_file)
+
+    def test_load_segmentation_missing_start(self):
+        db = """7.3	aachenskost#N	aachenskost	N		end=6&morph=aachen&type=prefix|end=8&morph=sk&start=6&type=root|end=11&morph=ost&start=8&type=suffix				{}
+"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        self.assertRaises(DerinetFileParseError, lexicon.load, db_file)
+
+    def test_load_segmentation_missing_end(self):
+        db = """7.3	aachenskost#N	aachenskost	N		morph=aachen&start=0&type=prefix|end=8&morph=sk&start=6&type=root|end=11&morph=ost&start=8&type=suffix				{}
+"""
+
+        lexicon = Lexicon()
+        db_file = io.StringIO(db)
+
+        self.assertRaises(DerinetFileParseError, lexicon.load, db_file)
+
+
+if __name__ == '__main__':
+    unittest.main()
