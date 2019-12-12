b4d6aa7e990d7f7c75cd52b3e8e99194dd4a650f
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2019-12-12 11:28:27 +0100

    Add treechecking module based on enumerated words
    In Czech, the spelling of i/y after certain consonants depends on
    whether the word is on a list of enumerated words or is derived from
    one. This derivation criterion can be used for checking the correctness
    of our trees: If we find a word with [bflmpsvz][yý] in the lemma, it
    must be enumerated or descended from one. If it is not, we are missing
    a link.

---
+++ tools/data-api/derinet2/derinet/modules/cs/checkenumeratedwords.py
@@ -0,0 +1,128 @@
+from derinet import Block, Format, Lexicon
+import re
+import argparse
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S')
+
+logger = logging.getLogger(__name__)
+
+
+def has_parent(lexeme, f, checked=None):
+    """
+    Ensures that lexeme satisfies f, or any of its ancestors do.
+    :param lexeme: The starting point of the search for ancestors.
+    :param f: The predicate function of Lexeme → truthy value.
+    :param checked: A set of lexemes which were already checked.
+    :return: f(ancestor), if there is an ancestor, for which f(ancestor)
+             returns a truthy value. False otherwise.
+    """
+
+    if checked is None:
+        checked = set()
+
+    # Break cycles.
+    if lexeme in checked:
+        return False
+    else:
+        checked.add(lexeme)
+
+    # First, check whether lexeme satisfies the proposition.
+    r = f(lexeme)
+    if r:
+        return r
+
+    # If it doesn't, search its parents depth-first.
+    for parent_rel in lexeme.parent_relations:
+        for parent in parent_rel.sources:
+            r = has_parent(parent, f, checked)
+            if r:
+                return r
+
+    return False
+
+
+class CheckEnumeratedWords(Block):
+    def __init__(self):
+        pass
+
+    def process(self, lexicon: Lexicon):
+        """
+        Verify that each Czech word in the lexicon, in which an ambiguous
+        consonant is followed by a hard y, is in the list of enumerated words
+        or is descended from one.
+        """
+
+        ambiguous_consonants = {
+            "b": frozenset(["by", "být", "bydlet", "obyvatel", "byt", "příbytek", "nábytek", "dobytek", "zbytek", "obyčej", "bystrý", "bylina", "kobyla", "býk", "Přibyslav", "babyka"]),
+            "f": frozenset(["fyzika", "refýž", "zefýr"]),
+            "l": frozenset(["slyšet", "mlýn", "blýskat", "polykat", "plynout", "plyn", "plýtvat", "vzlykat", "lysý", "lýtko", "lýko", "lyže", "pelyněk", "plyš", "slynout", "plytký", "vlys"]),
+            "m": frozenset(["my", "mýt ", "mýval", "myslet", "myslit", "mýlit", "hmyz", "myš", "hlemýžď", "mýtit", "vymýtit", "zamykat", "smýkat", "dmýchat", "chmýří", "nachomýtnout", "mýto", "mykat", "mys", "sumýš", "trubýš"]),
+            "p": frozenset(["pýcha", "pýchavka", "pytel", "pysk", "netopýr", "slepýš", "pyl", "opylovat", "kopyto", "klopýtat", "třpytit", "zpytovat", "pykat", "pýr", "pýřit", "čepýřit", "pýří", "pyj"]),
+            "s": frozenset(["syn", "sytý", "sýr", "sýrový", "syrový", "syrý", "sychravý", "usychat", "usýchat", "sýkora", "sýček", "sysel", "syčet", "sypat"]),
+            "v": frozenset(["vy", "vysoký", "výt", "výskat", "zvykat", "žvýkat", "vydra", "výr", "vyžle", "povyk", "výheň", "cavyky", "vyza", "kavyl"]),
+            "z": frozenset(["brzy", "jazyk", "nazývat", "ozývat", "vyzývat", "vzývat", "Ruzyně"])
+        }
+
+        for consonant, enumerated_words in ambiguous_consonants.items():
+            regex_string = consonant + "[yý]"
+            regex = re.compile(regex_string)
+
+            test_fn = lambda lexeme: lexeme if lexeme.lemma in enumerated_words else False
+
+            # For listing all such lexemes, including derived ones, change this to iter_lexemes.
+            # For a listing of only root lexemes, change it to iter_trees.
+            for lexeme in lexicon.iter_lexemes(sort=False):
+                if "foreign" not in lexeme.misc:
+                    logger.warning("Lexeme {} doesn't have the foreign tag specified".format(lexeme))
+                    continue
+
+                if lexeme.misc["foreign"]:
+                    # Ignore foreign lexemes.
+                    #logger.debug("Lexeme {} is foreign, ignoring".format(lexeme))
+                    continue
+
+                if re.search("^[A-ZÁÉÍÓÚŮĚŽŠČŘĎŤŇ]", lexeme.lemma):
+                    # Ignore proper names.
+                    continue
+
+                if (lexeme.pos == "A" and re.search("ý$", lexeme.lemma)) or (lexeme.pos == "D" and re.search("y$", lexeme.lemma)):
+                    # Don't take into account known suffixes and/or endings.
+                    continue
+
+                if regex.search(lexeme.lemma):
+                    # The combination is there, therefore, an enumerated word
+                    #  must be in the lexeme's ancestors.
+
+                    # Only print the roots of problematic subtrees – therefore,
+                    #  if the parent is problematic as well, ignore this lexeme
+                    #  and only print the parent (in a later / earlier iteration).
+                    parent_also_enumerated = False
+                    for parent_rel in lexeme.parent_relations:
+                        for parent in parent_rel.sources:
+                            if regex.search(parent.lemma):
+                                # The parent satisfies the condition as well.
+                                parent_also_enumerated = True
+                                break # Only breaks the inner loop, but whatever.
+
+                    if parent_also_enumerated:
+                        # Only print the root of broken subtrees.
+                        continue
+
+                    parent = has_parent(lexeme, test_fn)
+                    if parent:
+                        #logger.debug("Lexeme {} has enumerated parent {}".format(lexeme, parent))
+                        pass
+                    else:
+                        logger.warning("Lexeme {} doesn't have an enumerated word in its ancestors.".format(lexeme))
+                        if lexeme.parent_relations:
+                            logger.info("But it has a parent, which doesn't have the ambiguous consonant.")
+
+
+        return lexicon
+
+    @staticmethod
+    def parse_args(args):
+        return [], {}, args
