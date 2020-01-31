4da72d01cc296edf03cff6b158179db291f4caf5
Zdenek Zabokrtsky <zabokrtsky@ufal.mff.cuni.cz>
2020-01-31 08:12:35 +0100

    unifying processing of all three batches related to root allomorphs

---
+++ data/annotations/cs/2020_01_root_allomorphs_cleanup/induce_root_morpheme_boundaries/check_allomorph_overlaps.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+
+allomorphsets = []
+
+shortlemmas = []
+longlemmas = []
+
+def set2string (allomorphset):
+    return "{"+(",".join(allomorphset))+"}"
+
+
+for line in sys.stdin:
+    columns = line.rstrip().split('\t')
+    if columns[0] == "STARTOFCLUSTER":
+            if len(columns) == 1:  # A WARNING SHOULD BE ISSUED HERE
+                columns.append("")
+                
+            allomorphsets.append(set(columns[1].split(" ")))
+
+    if columns[0] == "ROOTNODE":
+        longlemmas.append(columns[2])        
+        shortlemmas.append(columns[3])
+
+for i1 in range(len(allomorphsets)):
+   for i2 in range(i1+1,len(allomorphsets)):
+       intersection = allomorphsets[i1].intersection(allomorphsets[i2])
+       if len(intersection) > 0:
+           print(str(len(intersection))+"\t"+
+              set2string(allomorphsets[i1].intersection(allomorphsets[i2])) + "\t" +
+                 set2string(allomorphsets[i1]) + "\t" + set2string(allomorphsets[i2]) + "\t" +
+                   longlemmas[i1] + "\t" + longlemmas[i2] + "\tSOLUTION:\tCOMMENT:")
+
+#                 {"+(",".join(allomorphsets[i1]))+"\t allomorph set B: "+(" ".join(allomorphsets[i2])))
+
+    
---
+++ data/annotations/cs/2020_01_root_allomorphs_cleanup/induce_root_morpheme_boundaries/find-root-morphemes.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use utf8;
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
+binmode(STDIN, ":utf8");
+
+my @root_allomorphs;
+
+
+sub penalty_for_spurious_occurrence {
+    my ($segmented) = @_;
+    return ($segmented =~ /\(t\)$|^(sub)?\(d\)o|^po\(d\)|^(pře|vy)\(d\)|^p\(řed\)|^(v)?o\(d\)|\([nt]\)[íěý]$|^(pro|pod)\(n\)a|^\(po\)|^\(n\)a|\(kův\)$|\(nos\)t(ní)?$|\(š\)í$|pseu\(d\)o|\(ší\)$|^\(vod\)v|\(s\)t$|\([tn]\)ost|^\(zn\)e|\(d\)lo$|^\(jist\)o|\(va\)$|^\(pro\)|^\(pr\)o|su\(per\)|\(v[aá]\)(t|ní|tí|ný|vající|jící|nost|cí)$|\(čk\)a$|\(vě\)$|\(nov\)[ýě]$|\(va\)$/);
+}
+   
+
+sub root_len {
+    my ($segmented) = @_;
+
+    if ($segmented =~ /\((.+)\)/) {
+	return (length($1) - 0.5 * penalty_for_spurious_occurrence($segmented));
+    }
+    
+    else {
+	return 0;
+    }
+}
+
+
+my $ignore_composite = 0;
+my $rootnode = 0;
+my $firstcluster = 1;
+
+while (<>) {
+    chomp;
+
+    s/","/\t/g;
+    s/,"/\t/g;
+    s/",/\t/g;
+    s/,,/\t\t/g;
+    s/,$/\t/g;
+    s/^"//;
+    s/"$//;
+    
+
+    if (/^\s*$/ or /Automatic roots/ ) {  } # empty lines ignored too
+
+    elsif (/Manual roots for (\S+\#VC):/ or /telef/) {
+	$ignore_composite = 1;
+    }
+    
+    elsif (/Manual roots for (\S+):/) {
+	$ignore_composite = 0;
+	my $root_lemma = $1;
+	my @columns = split /\t/;
+	my $allomorph_string = $columns[1];
+	if (@columns>3) {
+	    $allomorph_string = $columns[4];
+	}
+	
+	@root_allomorphs = grep {/[[:alpha:]]/} split / /, $allomorph_string;
+	$rootnode = 1;
+	
+	if (not $firstcluster) {
+	    print "ENDOFCLUSTER\n";
+	}
+	$firstcluster = 0;
+	    
+	print "STARTOFCLUSTER\t".(join " ",@root_allomorphs)."\n";
+    }
+
+    elsif (/\t[\$\!]/) { 
+	my ($shortlemma,$longlemma,$pos) = split /\t/;
+	if ($shortlemma and $longlemma) {
+	    print "STOPNODE\t$longlemma\n";
+	}
+    }
+
+    elsif ($ignore_composite) {
+
+    }
+    
+    elsif (/^(\S+)\t(\S+)\t([A-Z])/) {
+	my $short_lemma = $1;
+	my $long_lemma = $2;
+	my $pos = $3;
+
+	my $err = 0;
+	
+	my @solutions;
+	foreach my $root (@root_allomorphs) {
+	    my @solutions_for_this_root;
+	    while ($short_lemma =~ /$root/gi) {
+		my $from = $-[0];
+		my $to = $+[0];
+		my $segmented_lemma = substr($short_lemma,0,$from) . "(" . substr($short_lemma,$from,$to-$from) . ")" . substr($short_lemma,$to,100);
+		push @solutions_for_this_root, $segmented_lemma;
+
+	    }
+	    push @solutions, @solutions_for_this_root;
+	}
+
+	@solutions = sort { root_len($b) <=> root_len($a) } @solutions; 
+	
+	if ($err) { # already reported
+	}
+	
+	elsif (@solutions == 0) {
+	    print STDERR "Error: No root allomorph found\t out of '".(join " ",@root_allomorphs)."' in '$short_lemma'\n";
+	}
+	
+	elsif (@solutions > 1 and root_len($solutions[0]) == root_len($solutions[1])) {
+	    print STDERR "Error: Ambiguous segmentation 2: more matching allomorphs of the same length\t in '$short_lemma' out of ".(join " ",grep {root_len($_) == root_len($solutions[0])} @solutions)."\n";
+	}
+	
+	else {
+	    if ($rootnode) {
+		print "ROOTNODE\t$short_lemma\t$long_lemma\t$solutions[0]\n";
+	    }
+	    print "SEGMENTEDNODE\t$long_lemma\t$solutions[0]\n";
+	}
+	$rootnode = 0;
+
+    }
+
+    else {
+	print STDERR "Error: unrecognized line: $_ \n";
+    }
+
+    
+}
+
+print "ENDOFCLUSTER\n";
