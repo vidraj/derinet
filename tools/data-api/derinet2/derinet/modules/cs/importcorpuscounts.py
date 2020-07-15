fe823b41be94eb864a82f0c87c61f023724fc88f
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2020-07-15 17:29:41 +0200

    Refactor the corpus count import into functions

--- tools/data-api/derinet2/derinet/modules/cs/importcorpuscounts.py
+++ tools/data-api/derinet2/derinet/modules/cs/importcorpuscounts.py
@@ -17,43 +17,76 @@ class ImportCorpusCounts(Block):
     def __init__(self, fname):
         self.fname = fname
 
-    def process(self, lexicon: Lexicon):
-        corpus_size = 0
-
+    def gen_counts(self):
+        """
+        Generate parsed items from self.fname, returning the lemma, pos and
+        integer count for each item.
+        """
         with open(self.fname, "rt", encoding="utf-8", newline="\n") as f:
             for line in f:
                 fields = line.rstrip("\n").split("\t")
                 assert len(fields) == 3
                 lemma, pos, count = fields
-                count = int(count)
+                yield lemma, pos, int(count)
 
-                corpus_size += count
+    def fill_counts_from_file(self, lexicon: Lexicon):
+        """
+        Read self.fname and record all counts found therein to the appropriate
+        lexemes from lexicon as "CorpusCount" in misc. Return the sum of all
+        processed counts.
 
-                lexemes = lexicon.get_lexemes(lemma, pos)
+        If a lemma-pos pair from the count file is ambiguous (the lemma has
+        several homonymous lexemes), the same count is assigned to all of them,
+        but only counted once towards the returned total.
 
-                if not lexemes:
-                    logger.info("Lexeme for '{} {}' not found".format(lemma, pos))
-                    continue
+        If a lemma-pos pair is not found in the lexicon, the count is added to
+        the total anyway.
 
-                if len(lexemes) > 1:
-                    logger.warning("Lexeme for '{} {}' ambiguous, filling the count to all of them".format(lemma, pos))
+        Lexemes from lexicon not found in the file are untouched.
+        """
+        corpus_size = 0
 
-                for lexeme in lexemes:
-                    if "CorpusCount" in lexeme.misc:
-                        logger.warning("Lexeme {} already has corpus count filled"
-                                       "with value {}, will overwrite".format(
-                            lexeme,
-                            lexeme.misc["CorpusCount"]
-                        ))
+        for lemma, pos, count in self.gen_counts():
+            corpus_size += count
 
-                    lexeme.misc["CorpusCount"] = count
+            lexemes = lexicon.get_lexemes(lemma, pos)
 
-        # Initialize all unseen lexemes to 0.
+            if not lexemes:
+                logger.info("Lexeme for '{} {}' not found".format(lemma, pos))
+                continue
+
+            if len(lexemes) > 1:
+                logger.warning("Lexeme for '{} {}' ambiguous, filling the count to all of them".format(lemma, pos))
+
+            for lexeme in lexemes:
+                if "CorpusCount" in lexeme.misc:
+                    logger.warning("Lexeme {} already has corpus count filled"
+                                    "with value {}, will overwrite with {}".format(
+                        lexeme,
+                        lexeme.misc["CorpusCount"],
+                        count
+                    ))
+
+                lexeme.misc["CorpusCount"] = count
+
+        return corpus_size
+
+    def init_default_counts(self, lexicon: Lexicon):
+        """
+        Initialize the misc.CorpusCount of lexemes which don't have the key
+        yet to 0.
+        """
         for lexeme in lexicon.iter_lexemes():
             if "CorpusCount" not in lexeme.misc:
                 lexeme.misc["CorpusCount"] = 0
 
+
+    def process(self, lexicon: Lexicon):
+        # Fill in the absolute counts.
+        corpus_size = self.fill_counts_from_file(lexicon)
+        self.init_default_counts(lexicon)
         return lexicon
+
 
     @staticmethod
     def parse_args(args):
