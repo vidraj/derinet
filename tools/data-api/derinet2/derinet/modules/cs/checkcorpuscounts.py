7eb6fc1ec9a9665ef3ece6a6ffa1a3bc7c008df9
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2020-12-18 09:46:25 +0100

    Add a possibility to filter by count diff/ratio

--- tools/data-api/derinet2/derinet/modules/cs/checkcorpuscounts.py
+++ tools/data-api/derinet2/derinet/modules/cs/checkcorpuscounts.py
@@ -15,12 +15,37 @@ class CheckCorpusCounts(Block):
     itself, and report all corpus-attested words derived from unattested ones.
     """
 
+    def __init__(self, offset, ratio):
+        self.offset = offset
+        self.ratio = ratio
+
     def process(self, lexicon: Lexicon):
         for child in lexicon.iter_lexemes(sort=False):
             ccount = child.misc["corpus_stats"]["absolute_count"]
             for parent in child.all_parents:
                 pcount = parent.misc["corpus_stats"]["absolute_count"]
-                if pcount < ccount:
-                    print(parent, pcount, child, ccount, sep="\t")
+                if self.ratio * (self.offset + pcount) < ccount:
+                    score = self.ratio * (self.offset + pcount) / ccount
+                    print(parent, pcount, child, ccount, score, sep="\t")
 
         return lexicon
+
+    @staticmethod
+    def parse_args(args):
+        """Parse a list of strings containing the arguments, pick the relevant
+        ones from the beginning and leave the rest be. Return the parsed args
+        to this module and the unprocessed rest."""
+        parser = argparse.ArgumentParser(
+            prog=__class__.__name__,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+
+        parser.add_argument("--offset", type=float, default=0.0)
+        parser.add_argument("--ratio", type=float, default=1.0, help="Only print pairs with ratio * (offset + pc) < cc, where pc is parent's count and cc is child's count.")
+        # argparse.REMAINDER tells argparse not to be eager and to process only the start of the args.
+        parser.add_argument("rest", nargs=argparse.REMAINDER, help="A list of other modules and their arguments.")
+
+        args = parser.parse_args(args)
+
+        # Return *args to __init__, **kwargs to init and the unprocessed tail of arguments to other modules.
+        return [args.offset, args.ratio], {}, args.rest
