84578c9b70f1457b0090446d26536fc824ca32f8
Lukas Kyjanek <l.kyjanek@gmail.com>
2020-12-22 21:23:25 +0100

    add module for changig MorFflexCZ POS tagset to Universal POS tagset

---
+++ tools/data-api/derinet2/derinet/modules/cs/changepostags.py
@@ -0,0 +1,21 @@
+from derinet import Block, Format, Lexicon
+import argparse
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S')
+
+logger = logging.getLogger(__name__)
+
+
+class ChangePosTags(Block):
+    """
+    Change POS tags to Universal tagset.
+    """
+    def process(self, lexicon: Lexicon):
+        translate_pos = {'N': 'NOUN', 'A': 'ADJ', 'V': 'VERB', 'D': 'ADV'}
+        for lexeme in lexicon.iter_lexemes():
+            lexeme._pos = translate_pos[lexeme.pos]
+
+        return lexicon
