257d87d669c1c78878ba38193fb46d53d0893e03
Lukas Kyjanek <l.kyjanek@gmail.com>
2021-02-24 10:21:01 +0100

    fix module for uploading variants to DeriNet

--- tools/data-api/derinet2/derinet/modules/cs/addvariants.py
+++ tools/data-api/derinet2/derinet/modules/cs/addvariants.py
@@ -72,48 +72,71 @@ class AddVariants(Block):
                 repre = variants[0]
                 variants = variants[1:]
 
-                # add variant relations, solve problems
-                for varian_lemma in variants:
+                # add variant relations
+                for nonrepre in variants:
 
-                    # repre. lemma is connected to one of non-repre. lemmas
-                    if repre.parent and repre.parent in variants:
+                    # (i) repre. is in the subtree of the non-repre lemma
+                    if repre in list(nonrepre.iter_subtree()):
+                        logger.info('Relation between lexeme {} and '
+                                    'lexeme {} was deleted.'
+                                    .format(repre, repre.parent))
                         repre.parent_relation.remove_from_lexemes()
-                        logger.info('Relation between lexeme {} and lexeme {} '
-                                    'was deleted.'.format(repre, repre.parent))
 
-                    # repre. is connected to one of non-repre. lemmas's child
-                    for nonrepre in variants:
-                        if repre.parent in list(nonrepre.iter_subtree()):
-                            repre.parent_relation.remove_from_lexemes()
-                            logger.info('Relation between lexeme {} and '
-                                        'lexeme {} was deleted.'
-                                        .format(repre, repre.parent))
+                    # (ii) nonrepre is in the subtree of the repre lemma
+                    if nonrepre in list(repre.iter_subtree()):
+                        logger.info('Relation between lexeme {} and '
+                                    'lexeme {} was deleted.'
+                                    .format(nonrepre, nonrepre.parent))
+                        nonrepre.parent_relation.remove_from_lexemes()
 
-                    # connect non-repre. lemmas + their subtrees to repre lemma
-                    for nonrepre in variants:
-
-                        # delete parent relation of non-repre. lemma
-                        if nonrepre.parent:
-                            nonrepre.parent_relation.remove_from_lexemes()
-                            logger.info('Relation between lexeme {} and '
-                                        'lexeme {} was deleted.'
-                                        .format(nonrepre, nonrepre.parent))
-
-                        # reconnect children of non-repre. lemma to repre lemma
-                        for child in nonrepre.children:
-                            child.parent_relation.remove_from_lexemes()
-                            lexicon.add_derivation(source=repre, target=child)
-                            logger.info('Relation between lexeme {} and '
-                                        'lexeme {} was reconnected as '
-                                        'relation between lexeme {} and '
-                                        'lexeme {}.'
-                                        .format(nonrepre, child, repre, child))
-
+                    # (iii) connect non-repre + their subtrees -> repre lemma
+                    # non-repre lemma does not have parent
+                    if not nonrepre.parent:
                         # connect non-repre. lemma to repre lemma
-                        lexicon.add_variant(source=repre, target=nonrepre)
-                        logger.info('Relation between lexeme {} and lexeme {} '
-                                    'was added and labeled as Variant.'
+                        logger.info('Relation between lexeme {} and lexeme'
+                                    ' {} was added and labeled as Variant.'
                                     .format(repre, nonrepre))
+                        lexicon.add_variant(source=repre, target=nonrepre)
+
+                    # non-repre lemma does have parent as well as repre
+                    elif repre.parent:
+                        logger.info('Relation between lexeme {} '
+                                    'and lexeme {} was deleted.'
+                                    .format(nonrepre, nonrepre.parent))
+                        nonrepre.parent_relation.remove_from_lexemes()
+                        logger.info('Relation between lexeme {} and lexeme'
+                                    ' {} was added and labeled as Variant.'
+                                    .format(repre, nonrepre))
+                        lexicon.add_variant(source=repre, target=nonrepre)
+
+                    # non-repre lemma does have parent and repre does not
+                    elif not repre.parent:
+                        # connect repre lemma to non-repre parent
+                        logger.info('Relation between lexeme {} and '
+                                    'lexeme {} was reconnected as '
+                                    'relation between lexeme {} and '
+                                    'lexeme {}.'
+                                    .format(nonrepre, nonrepre.parent,
+                                            repre, nonrepre.parent))
+                        lexicon.add_derivation(source=nonrepre.parent,
+                                               target=repre)
+                        nonrepre.parent_relation.remove_from_lexemes()
+                        logger.info('Relation between lexeme {} and lexeme'
+                                    ' {} was added and labeled as Variant.'
+                                    .format(repre, nonrepre))
+                        lexicon.add_variant(source=repre, target=nonrepre)
+
+                    # (iv) reconnect non-repre. children to repre lemma
+                    for child in nonrepre.children:
+                        logger.info('Relation between lexeme {} and '
+                                    'lexeme {} was reconnected as '
+                                    'relation between lexeme {} and '
+                                    'lexeme {}.'
+                                    .format(nonrepre, child,
+                                            repre, child))
+                        child.parent_relation.remove_from_lexemes()
+                        lexicon.add_derivation(source=repre,
+                                               target=child)
 
         return lexicon
 
