78b9bd3324aecde5631b6876cb400f3d2f8cc932
Zdenek Zabokrtsky <zabokrtsky@ufal.mff.cuni.cz>
2022-08-03 15:01:33 +0200

    selection of random samples from the Sigmorphon 2022 shared task on morphosegmentation

---
+++ data/annotations/multiling-segm-from-sigmorphon/select-random-n-lines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import random
+
+infile = sys.argv[1]
+howmanylines = int(sys.argv[2])
+
+sys.stderr.write("Randomly selecting "+str(howmanylines)+" from "+infile + "\n")
+
+random.seed(1)
+
+with open(infile) as f:
+    lines = random.sample(f.readlines(),howmanylines)
+    print(*lines,sep='')
