48f528d9deed29ed9b52cad78b686195591cea8d
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2024-05-05 00:20:04 +0200

    Don't integrate process_scenario fully as module
    This reverts commit 104830ad9fba024048a4a88d6c3af44ccd5a57fa.
    There is no need to break backwards compatibility in this way.

--- tools/data-api/derinet2/derinet/process_scenario.py
+++ tools/data-api/derinet2/derinet/process_scenario.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python3
+
 """Process a derivational network using a scenario specified as individual args."""
+import derinet.modules
 import sys
 import importlib
 import logging
 import argparse
 
-from . import modules
-from .scenario import Scenario
-from .block import Block
+from derinet.scenario import Scenario
+from derinet.block import Block
 
 
 logging.basicConfig(level=logging.DEBUG,
@@ -200,3 +202,7 @@ def main():
     scenario.process(keep_going=main_args.keep_going)
 
     return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
