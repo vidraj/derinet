9d0fde002a67a454aac0499d9cfd8c69e2b7ef5c
Jonáš Vidra <vidra@ufal.mff.cuni.cz>
2024-05-27 10:22:29 +0200

    Don't fail when a lexeme has no corpus count
    If a lexeme lacks the corpus count annotation, don't die, but instead
    consider the lexeme to be attested and continue.

--- tools/data-api/derinet2/derinet/modules/cs/deleteunattestedleaves.py
+++ tools/data-api/derinet2/derinet/modules/cs/deleteunattestedleaves.py
@@ -15,11 +15,22 @@ class DeleteUnattestedLeaves(Block):
     Filter the database so that no unattested leaves remain.
     """
 
+    def corpus_count(self, lexeme):
+        if "corpus_stats" in lexeme.misc:
+            cs = lexeme.misc["corpus_stats"]
+            if "absolute_count" in cs:
+                return cs["absolute_count"]
+
+        # We can return 0 or not-0, depending on what the default should
+        #  be. Let's say that lexemes with missing annotation are
+        #  considered to be attested.
+        return 1
+
     def delete_children(self, lexicon, lexeme):
         for child in lexeme.children:
             self.delete_children(lexicon, child)
 
-        if lexeme.misc["corpus_stats"]["absolute_count"] == 0 and len(lexeme.children) == 0:
+        if self.corpus_count(lexeme) == 0 and len(lexeme.children) == 0:
             lexicon.delete_lexeme(lexeme, delete_relations=True)
 
     def process(self, lexicon: Lexicon):
