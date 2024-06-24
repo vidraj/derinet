7d7f9fb18adb311fbc1834d4411e1563c55da168
Viktória Brezinová <vikibrezinova@gmail.com>
2024-06-24 15:12:35 +0200

    change writing Etymology to misc and propagate through tree

--- tools/data-api/derinet2/derinet/modules/cs/addetymology.py
+++ tools/data-api/derinet2/derinet/modules/cs/addetymology.py
@@ -1,6 +1,7 @@
 from derinet import Block, Lexicon
 import argparse
 import logging
+import json
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -9,24 +10,26 @@ logging.basicConfig(level=logging.DEBUG,
 logger = logging.getLogger(__name__)
 
 
-def read_etym_data(filename: str) -> dict[str, list[str]]:
+def read_etym_data(filename: str) -> dict[str, (list[str], bool)]:
     '''
     Parses a tsv file containing words and their etymological information.
-
+    
     Parameters:
     - filename (str): The path to the tsv file containing data.
-
+    
     Returns:
-    - dict[str, list[str]]: A dictionary where each key is a word and each value 
-    is a list of abbreviations of countries from which the word came to czech.
+    - dict[str, (list[str], bool)]: A dictionary where each key is a word and each value 
+    is a tuple, containig a list of abbreviations of countries from which the word came 
+    to czech and a bool value describing whether a word is loan or not.
     '''
     etym_data = {}
     with open(filename, 'r') as etym_file:
         for line in etym_file.readlines():
             line = line.split('\t')
-            word = line[0].strip()
+            word = line[0]
             etymology = line[1].split(',')
-            etym_data[word] = etymology
+            loan = line[2]
+            etym_data[word] = (etymology, loan)
     return etym_data
 
 
@@ -39,17 +42,23 @@ class AddEtymology(Block):
     def process(self, lexicon: Lexicon):
         '''
         Writes etymological information to root lexeme
-        to new feature Etymology as a list of abbreviations
-        of countries.
+        to new feature Etymology in misc in json format.
+        Rewrites Loanword feature in feats accordingly.
         '''
         data = read_etym_data(self.fname)
-        for word, etymology in data.items():
-            lexemes = lexicon.get_lexemes(lemma=word)
-            for lexeme in lexemes:
-                root_lexeme = lexeme.get_tree_root()
-                if 'Etymology' not in root_lexeme.feats:
-                    root_lexeme.add_feature('Etymology', etymology)
-
+        for word, (etymology, loanmark) in data.items():
+            if loanmark == 'True':
+                lexemes = lexicon.get_lexemes(lemma=word)
+                for lexeme in lexemes:
+                    root_lexeme = lexeme.get_tree_root()
+                    if 'Etymology' not in root_lexeme.misc:
+                        # propagate to all lexemes in a tree
+                        for derived_lexeme in root_lexeme.iter_subtree():
+                            derived_lexeme.misc['Etymology'] = json.dumps(etymology[:3])
+                            if 'Loanword' not in derived_lexeme.feats:
+                                derived_lexeme.add_feature('Loanword', loanmark)
+                            else:
+                                derived_lexeme.feats['Loanword'] = loanmark
         return lexicon
 
     @staticmethod
