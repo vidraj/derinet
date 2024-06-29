1e11825f80c1c94f9637cfa196c12cdbc870efcc
iml-r <emilsvob@gmail.com>
2024-06-30 00:44:15 +0200

    add disambiguation of homophones

---
+++ tools/data-api/derinet2/derinet/modules/cs/renameaffixoidtoneocon.py
@@ -0,0 +1,31 @@
+from derinet import Block, Lexicon
+from derinet.utils import DerinetCycleCreationError
+import argparse
+import logging
+import re
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S')
+
+logger = logging.getLogger(__name__)
+
+def techlemma_to_lemma(techlemma):
+    """Cut off the technical suffixes from the string techlemma and return the raw lemma"""
+    shortlemma = re.sub("[_`].+", "", techlemma)
+    lemma = re.sub("-\d+$", "", shortlemma)
+    return lemma
+
+
+class RenameAffixoidToNeocon(Block):
+    def process(self, lexicon: Lexicon):
+        """
+        This simple block renames all Affixoid POS to Neocon to avoid annoying Emil Svoboda's dissertation
+        reviewers.
+        """
+
+        for lexeme in lexicon.iter_lexemes():
+            if lexeme.pos == "Affixoid":
+                lexeme.pos = "Neocon"
+
+        return lexicon
