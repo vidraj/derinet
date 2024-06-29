157f9acd70a0201edbbae87327581e66ecb386c9
iml-r <emilsvob@gmail.com>
2024-06-29 02:22:08 +0200

    Commit remaining changes before switching branches

--- modules/cs/renameaffixoidtoneocon.py
+++ modules/cs/renameaffixoidtoneocon.py
@@ -16,7 +16,7 @@ def techlemma_to_lemma(techlemma):
     lemma = re.sub("-\d+$", "", shortlemma)
     return lemma
 
-class AddPronouns(Block):
+class RenameAffixoidToNeocon(Block):
     def __init__(self, fname):
         # The arguments to __init__ are those that the parse_args method (below) returns.
         self.fname = fname
