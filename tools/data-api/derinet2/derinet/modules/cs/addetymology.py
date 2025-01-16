0368071ea99052302e186ecce24671932e109458
Viktória Brezinová <52294703+vikibrezinova@users.noreply.github.com>
2025-01-16 16:53:06 +0100

    Update addetymology.py to not write to compounds and choose the closest etymological info to the root.

--- tools/data-api/derinet2/derinet/modules/cs/addetymology.py
+++ tools/data-api/derinet2/derinet/modules/cs/addetymology.py
@@ -3,11 +3,10 @@ import argparse
 import logging
 import json
 
-
 logger = logging.getLogger(__name__)
 
 
-def read_etym_data(filename: str) -> dict[str, (list[str], bool)]:
+def read_etym_data(filename: str) -> dict[str, (list[str], str)]:
     '''
     Parses a tsv file containing words and their etymological information.
     
@@ -15,17 +14,17 @@ def read_etym_data(filename: str) -> dict[str, (list[str], bool)]:
     - filename (str): The path to the tsv file containing data.
     
     Returns:
-    - dict[str, (list[str], bool)]: A dictionary where each key is a word and each value 
+    - dict[str, (list[str], str)]: A dictionary where each key is a word and each value 
     is a tuple, containig a list of abbreviations of countries from which the word came 
-    to czech and a bool value describing whether a word is loan or not.
+    to czech and a string value loan or native.
     '''
     etym_data = {}
-    with open(filename, 'r') as etym_file:
+    with open(filename, 'r', encoding="utf8") as etym_file:
         for line in etym_file.readlines():
             line = line.split('\t')
             word = line[0]
             etymology = line[1].split(',')
-            loan = line[2]
+            loan = line[2].strip()
             etym_data[word] = (etymology, loan)
     return etym_data
 
@@ -36,6 +35,32 @@ class AddEtymology(Block):
         # The arguments to __init__ are None (returned from below).
         self.fname = fname
 
+    def write_etym_data(self, lexeme, etymology, loanmark):
+        '''
+        Writes etymological information to root lexeme
+        to new feature Etymology in misc in json format.
+        Rewrites Loanword feature in feats accordingly.
+        Then recursively propagates the information to all lexemes in a subtree,
+        but not to compounds.
+        '''
+        # loanword
+        if loanmark == 'loan':
+            lexeme.misc['Etymology'] = json.dumps(etymology)
+            if 'Loanword' not in lexeme.feats:
+                lexeme.add_feature('Loanword', 'True')
+            else:
+                lexeme.feats['Loanword'] = 'True'
+        # native word
+        else:
+            if 'Loanword' not in lexeme.feats:
+                lexeme.add_feature('Loanword', 'False')
+            else:
+                lexeme.feats['Loanword'] = 'False'
+        # propagate to all lexemes in a subtree that are not compound
+        for child in lexeme.children:
+            if 'is_compound' in child.misc and child.misc['is_compound'] == False:
+                self.write_etym_data(child, etymology, loanmark)
+
     def process(self, lexicon: Lexicon):
         '''
         Writes etymological information to root lexeme
@@ -43,19 +68,36 @@ class AddEtymology(Block):
         Rewrites Loanword feature in feats accordingly.
         '''
         data = read_etym_data(self.fname)
+        # collect etymological information for each root lexeme
+        roots_with_etym_info = {}
         for word, (etymology, loanmark) in data.items():
-            if loanmark == 'True':
-                lexemes = lexicon.get_lexemes(lemma=word)
-                for lexeme in lexemes:
-                    root_lexeme = lexeme.get_tree_root()
-                    if 'Etymology' not in root_lexeme.misc:
-                        # propagate to all lexemes in a tree
-                        for derived_lexeme in root_lexeme.iter_subtree():
-                            derived_lexeme.misc['Etymology'] = json.dumps(etymology[:3])
-                            if 'Loanword' not in derived_lexeme.feats:
-                                derived_lexeme.add_feature('Loanword', loanmark)
-                            else:
-                                derived_lexeme.feats['Loanword'] = loanmark
+            lexemes = lexicon.get_lexemes(lemma=word)
+            for lexeme in lexemes:
+                root_lexeme = lexeme.get_tree_root()
+                # depth of the lexeme in the tree
+                depth = 0
+                while lexeme.parent is not None:
+                    lexeme = lexeme.parent
+                    depth += 1
+                if root_lexeme.lemma in roots_with_etym_info:
+                    roots_with_etym_info[root_lexeme.lemma].append((depth, etymology, loanmark))
+                else:
+                    roots_with_etym_info[root_lexeme.lemma] = [(depth, etymology, loanmark)]
+
+        # write etymological information to root lexemes and propagate it
+        for root_lexeme, etym_info in roots_with_etym_info.items():
+            root_lexemes = lexicon.get_lexemes(lemma=root_lexeme)
+            for root in root_lexemes:
+                # only one etymological information for the tree
+                if len(etym_info) == 1:
+                    (depth, etymology, loanmark) = etym_info[0]
+                    self.write_etym_data(root, etymology, loanmark)
+                # multiple etymological information for the tree
+                # choose the one with the smallest depth (closest to the root)
+                else:
+                    etym_info.sort()
+                    (depth, etymology, loanmark) = etym_info[0]
+                    self.write_etym_data(root, etymology, loanmark)
         return lexicon
 
     @classmethod
